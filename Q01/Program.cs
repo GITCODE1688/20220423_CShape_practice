dfa18649abd666cf731d1b19046bdf086584e4d7
GITCODE1688 <31927606+GITCODE1688@users.noreply.github.com>
2022-04-23 12:11:17 +0800

    .

--- Q01/Program.cs
+++ Q01/Program.cs
@@ -10,38 +10,6 @@ namespace Q01
         {
         static void Main(string[] args)
             {
-            int score;
-            Console.WriteLine ("請輸入分數：");
-            score = int.Parse ( Console.ReadLine ( ) );
-            Console.WriteLine ( evaluation(score));
-            Console.ReadKey ( );
-
             }
-
-        static String evaluation(int score )
-            {
-            string result = "";
-
-            if (score >= 90)
-                {
-                result = "考試優秀";
-                }
-            else if (score >= 80)
-                {
-                result = "考試良好";
-                }
-            else if (score >= 60)
-                {
-                result = "考試及格";
-                }
-            else
-                {
-                result = "下次繼續努力";
-                }
-
-            return result;
-
-            }
-
         }
     }
