9cc9f45d7558fd951f24b227ba45bfb8afbf089f
GITCODE1688 <31927606+GITCODE1688@users.noreply.github.com>
2022-04-23 12:29:13 +0800

    Q01(ok)

--- Q01/Program.cs
+++ Q01/Program.cs
@@ -10,6 +10,50 @@ namespace Q01
         {
         static void Main(string[] args)
             {
+            string inputScore;
+            int score;
+            int number;
+
+            Console.WriteLine ( "請輸入分數：" );
+            inputScore =  Console.ReadLine ( ) ;
+            if (int.TryParse ( inputScore, out number ))
+                {
+                score = int.Parse ( inputScore );
+                Console.WriteLine ( evaluation ( score ) );
+                }
+            else
+                {
+                Console.WriteLine ( "請輸入數值！！" );
+                }
+
+            Console.ReadKey ( );
+
             }
+
+        static String evaluation(int score)
+            {
+            string result = "";
+
+            if (score >= 90)
+                {
+                result = "考試優秀";
+                }
+            else if (score >= 80)
+                {
+                result = "考試良好";
+                }
+            else if (score >= 60)
+                {
+                result = "考試及格";
+                }
+            else
+                {
+                result = "下次繼續努力";
+                }
+
+            return result;
+
+            }
+
         }
     }
